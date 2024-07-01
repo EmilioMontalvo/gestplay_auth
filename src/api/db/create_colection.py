1f43279012bc89f5506f82889c9862855aac205d
EmilioMontalvo <80224370+EmilioMontalvo@users.noreply.github.com>
2024-06-30 21:13:35 -0500

    feat: crud operations

---
+++ src/api/db/create_colection.py
@@ -0,0 +1,53 @@
+from mongo_db import get_db
+from bson import ObjectId
+
+async def create_document(collection_name: str, document: dict):
+    try:
+        db = await get_db()
+        collection = db[collection_name]
+        result = await collection.insert_one(document)
+        return result
+    except Exception as e:
+        print("Error al escribir en la base de datos:", e)
+        return None
+
+#delete_document
+async def delete_document(collection_name: str, id: str):
+    try:
+        db = await get_db()
+        collection = db[collection_name]
+        result = await collection.delete_one({"_id": ObjectId(id)})        
+        if result.deleted_count == 1:
+            return True 
+        else:
+            return False 
+
+    except Exception as e:
+        print("Error al eliminar carta por ID:", e)
+        return None
+
+#find_document by id
+async def find_document(collection_name: str, id: str):
+    try:
+        db = await get_db()
+        collection = db[collection_name]
+        result = await collection.find_one({"_id": ObjectId(id)})
+        return result
+    except Exception as e:
+        print("Error al buscar documento por ID:", e)
+        return None
+
+#update_document
+async def update_letter_id(collection_name: str,id: str, document):
+    try:
+        db = await get_db()
+        collection = db[collection_name]
+        result = await collection.update_one({"_id": ObjectId(id)}, {"$set": document})
+        
+        if result.modified_count == 1:
+            return result
+        else:
+            return None
+    except Exception as e:
+        print("Error al actualizar carta por ID:", e)
+        return None
---
+++ src/api/schemas/game_profile.py
@@ -0,0 +1,30 @@
+from pydantic import BaseModel, Field, field_validator, ValidationError
+from bson import ObjectId
+
+class GameProfile(BaseModel):
+    local_id: str
+    first_name: str
+    last_name: str
+    image_path: str
+    max_click_level: int
+    max_cursor_level: int
+
+
+class GameProfileInDB(BaseModel):
+    id: str = Field(..., example="6122a42e67a51d001555de9a")
+    letter: GameProfile
+
+    @field_validator('id')
+    def validate_object_id(cls, v):
+        try:
+            ObjectId(v)
+        except Exception:
+            raise ValueError('Invalid ObjectId')
+        return v
+    
+    @classmethod
+    def from_mongo(cls, data):
+        return cls(id=str(data["_id"]), letter=GameProfile(content=data["content"]))
+
+    def to_mongo(self):
+        return self.model_dump()
