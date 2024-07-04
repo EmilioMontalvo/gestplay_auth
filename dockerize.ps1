3b0e0189897545516831784059419a7f2fb29a26
EmilioMontalvo <80224370+EmilioMontalvo@users.noreply.github.com>
2024-07-04 11:20:25 -0500

    buiid: docker compose name update

--- dockerize.ps1
+++ dockerize.ps1
@@ -15,7 +15,7 @@ if (-not $envVars.ContainsKey("DOCKER_USERNAME") -or -not $envVars.ContainsKey("
 
 $dockerUsername = $envVars["DOCKER_USERNAME"]
 $dockerPassword = $envVars["DOCKER_PASSWORD"]
-$imageName = "$dockerUsername/gestplay-auth:latest"
+$imageName = "$dockerUsername/gestplay-backend:latest"
 
 Write-Host "Login..."
 try {
@@ -34,6 +34,26 @@ try {
     Write-Host "Failed to build Docker image. Exiting script."
     exit 1
 }
+
+Write-Host "Tagging Docker image..."
+try {
+    docker tag auth_service:latest $imageName
+    Write-Host "Docker image tagged successfully."
+} catch {
+    Write-Host "Failed to tag Docker image. Exiting script."
+    exit 1
+}
+
+# Verificar si la imagen se construyó y etiquetó correctamente
+Write-Host "Verifying Docker image..."
+try {
+    docker image inspect $imageName
+    Write-Host "Docker image exists locally."
+} catch {
+    Write-Host "Docker image does not exist locally. Exiting script."
+    exit 1
+}
+
 
 Write-Host "Pushing Docker image..."
 try {
